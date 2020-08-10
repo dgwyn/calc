6a6c93d4ecef899c6244b99c376193c6e5d3743d
David Nghiem <lamda@defn.sh>
2020-08-10 09:07:56 -0700

    added new tests

--- main_test.go
+++ main_test.go
@@ -10,4 +10,14 @@ func TestSum(t *testing.T) {
 	if sm != want {
 		t.Errorf("Fail! sm = %d, want %d", sm, want)
 	}
+	sm = sum(0, 0)
+	want = 0
+	if sm != want {
+		t.Errorf("Fail! sm = %d, want %d", sm, want)
+	}
+	sm = sum(105,209)
+	want = 314 
+	if sm != want {
+		t.Errorf("Fail! sm = %d, want %d", sm, want)
+	}
 }
